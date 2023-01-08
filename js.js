b79875158c707707b53b6531c635c93b75fdef44
Gbiruk <getachewbiruk990@gmail.com>
2023-01-07 23:20:23 -0800

    webdesign1

---
+++ js.js
@@ -0,0 +1,31 @@
+console.log("i like pizza");
+window.alert("this is alert for you and me ");
+// this is single line comment 
+/* this is multi line comment
+and better than single 
+line comment
+*/
+// variable 
+// variable has two step 
+// first = declaration (let, const, var)
+// assignment
+let age;
+age=21;
+let name="biruk";
+let student=true;
+console.log("my name is"+ name);
+console.log("i am "+age);
+console.log(student);
+document.getElementById("p1").innerHTML="this clonned version of paragraph one" + name;
+// operation preccedence
+//parentesis
+// exponent
+//multiplication amd division
+//substracton and addition
+let username= window.prompt("what is your name");
+console.log("hellow", username);
+let username2;
+document.getElementById("on").onclick=function(){
+username2=document.getElementById("mytext").value;
+console.log(username2);
+}
